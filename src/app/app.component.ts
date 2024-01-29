202d367eb604d26215acfe09c3011732b0f026c5
aSimpleUserOrSomeone <maqubmaz@gmail.com>
2024-01-29 18:02:46 +0100

    Initial changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FieldComponent } from './field/field.component';
+import _positions from '../assets/positions.json';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FieldComponent, NgFor],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'chineese-game';
+  passedValue = 'passed text';
+  positions: any;
+
+  constructor() {
+    this.positions = _positions;
+  }
+
+  ngOnInit() {
+    console.log(this.positions);
+  }
 }
